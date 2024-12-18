a2d862f31cd68d092f4b41f949e2efda3121aa0b
Jakub Rożek <jrozek@datawire.io>
2024-12-18 11:29:34 +0100

    Remove getClusterID util function
    Signed-off-by: Jakub Rożek <jrozek@datawire.io>

--- pkg/k8sapi/namespaceid.go
+++ pkg/k8sapi/namespaceid.go
@@ -6,13 +6,6 @@ import (
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// GetClusterID returns the cluster ID for the given cluster.  If there is an error, it still
-// returns a usable ID along with the error.
-func GetClusterID(ctx context.Context) (clusterID string, err error) {
-	// Get the ID of the "default" Namespace.
-	return GetNamespaceID(ctx, "default")
-}
-
 // GetNamespaceID returns the uuid for a given namespace.  If there is an error, it still
 // returns a usable ID along with the error.
 func GetNamespaceID(ctx context.Context, namespace string) (clusterID string, err error) {
